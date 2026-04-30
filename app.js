164078109c2c5c4f77a948e059d525e5f27de902
SUDHEEKSHA007 <tkjsudheeksha@gmail.com>
2026-04-30 14:17:18 +0000

    Upgrade: Docker + CI/CD

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const http = require("http");
 
+const PORT = process.env.PORT || 3000;
+
 const server = http.createServer((req, res) => {
-  res.end("Hello from SSH deployed app 🚀");
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({
+    message: "Production-ready Node App 🚀",
+    status: "running",
+    timestamp: new Date()
+  }));
 });
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
